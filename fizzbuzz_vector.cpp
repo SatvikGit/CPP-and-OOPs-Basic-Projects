93c18b94f760d55f4c1436c5cea9e09458f2a5e1
Satvik Pathak <pathak.satvik230@gmail.com>
2023-11-18 12:26:10 +0530

    commit

--- fizzbuzz_vector.cpp
+++ fizzbuzz_vector.cpp
@@ -1,9 +1,41 @@
+#include <cassert>
 #include <iostream>
+#include <string_view>
 #include <vector>
 
+void fizzbuzz(int number)
+{
+    using namespace std::literals::string_view_literals;
+
+    const std::vector divisors { 3, 5, 7, 11, 13, 17, 19 };
+	const std::vector words { "fizz"sv, "buzz"sv, "pop"sv, "bang"sv, "jazz"sv, "pow"sv, "boom"sv };
+
+    assert(std::size(divisors) == std::size(words) && "fizzbuzz: array sizes don't match");
+
+    // Loop through each number between 1 and number (inclusive)
+    for (int i = 1; i <= number; i++)
+    {
+        bool is_div{};
+
+        for (std::size_t j = 0; j < divisors.size(); j++)
+        {
+            // Check the current number against each possible divisor
+            if ((i % divisors[j]) == 0)
+            {
+                std::cout << words[j];
+                is_div = true;
+            }
+        }
+
+        // If there were no divisors
+        if (!is_div)
+            std::cout << i;
+
+        std::cout << '\n';
+    }
+}
+
 int main(){
-    std::vector<int> divisor{3, 5, 7, 11, 13, 17, 19};
-    std::vector<std::string_view> words{"fizz", "pop", "pop",  "bang", "jazz", "pow", "boom"};
-    
+    fizzbuzz(150);
     return 0;
 }
